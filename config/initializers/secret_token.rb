b7e0514338ba1751c2df9820332f3ef2865b1eda
pavank <pavank@flipkart.com>
2013-12-20 12:22:05 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '50e10d4bbb6bee782d1f95d99237c086a09c79c1eabb3ec162b203c9e1170efa34ac2b65ce6cee4028a1aaf5cb74cb7f1bb720737a62e556455d06d8d7da2878'
